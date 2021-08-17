9a40eb4ccc7f3b9790622c857ceaf3012c790f22
Jacopo Panerati <jacopo.panerati@utoronto.ca>
2021-08-16 22:41:50 -0400

    Dependencies and gitignore

--- setup.py
+++ setup.py
@@ -2,5 +2,20 @@ from setuptools import setup
 
 setup(name='safe_control_gym',
     version='0.0.1',
-    install_requires=['numpy', 'Pillow', 'matplotlib', 'gym', 'pybullet']
+    install_requires=[
+        'matplotlib', 
+        'scikit-optimize', 
+        'munch', 
+        'pyyaml', 
+        'imageio', 
+        'dict-deep', 
+        'gym', 
+        'torch', 
+        'tensorboard', 
+        'casadi', 
+        'pybullet', 
+        'gpytorch', 
+        'cvxpy', 
+        'pytope', 
+        'Mosek']
 )
