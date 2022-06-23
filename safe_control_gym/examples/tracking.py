e82e4e90da92f9ba7be92b7e05e7d4bd5699a54b
michelangelo Fantini <miche@MacBook-Air-di-michelangelo.local>
2022-06-23 10:36:36 -0400

    fix errors

--- safe_control_gym/examples/tracking.py
+++ safe_control_gym/examples/tracking.py
@@ -28,9 +28,8 @@ def main():
     
     # Set iterations and episode counter.
     ITERATIONS = int(config.quadrotor_config['episode_len_sec']*config.quadrotor_config['ctrl_freq'])
-    miche = True
     #detect if script is being run is a TTY or PTY
-    if not sys.stdout.isatty() or miche:
+    if not sys.stdout.isatty():
         config.quadrotor_config['gui'] = False
         config.quadrotor_config['record'] = True
 
