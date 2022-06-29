a57bfe99896ffed659d4f5fbdee00308c46d5270
Federico-PizarroBejarano <federico.pizarrobejarano@mail.utoronto.ca>
2022-06-29 10:28:42 -0400

    Fixing training script

--- experiments/annual_reviews/figure8/create_unsafe_ppo_model.sh
+++ experiments/annual_reviews/figure8/create_unsafe_ppo_model.sh
@@ -4,8 +4,8 @@
 rm -r -f ./unsafe_ppo_temp_data/
 
 # Train the unsafe PPO controller/agent.
-python3 ../../main.py --algo ppo --task cartpole --overrides ./config_overrides/unsafe_ppo_config.yaml --output_dir ./ \
-                    --tag unsafe_ppo_temp_data/ --kv_overrides task_config.ctrl_freq=10 task_config.pyb_freq=1000 --seed 2
+python3 ../../main.py --algo ppo --task cartpole --overrides ./config_overrides/ppo_config.yaml --output_dir ./ \
+                    --tag unsafe_ppo_temp_data/ --kv_overrides task_config.ctrl_freq=10 task_config.info_in_reset=True task_config.pyb_freq=1000 --seed 2
 
 # Backup the unsafe PPO model.
 cp ./unsafe_ppo_model/unsafe_ppo_model_30000.pt ./unsafe_ppo_model/bak_unsafe_ppo_model_30000.pt 
