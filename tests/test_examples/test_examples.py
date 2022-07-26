16ad5e4d879af45dbb126f4a20629912aa454c9f
Federico-PizarroBejarano <federico.pizarrobejarano@mail.utoronto.ca>
2022-07-26 12:57:00 -0400

    Reorganizing tests to have algo then task, as is standard

--- tests/test_examples/test_examples.py
+++ tests/test_examples/test_examples.py
@@ -4,9 +4,9 @@ from examples.tracking import run as tracking_run
 from examples.verbose_api import run as verbose_run
 
 def test_tracking():
-    sys.argv[1:] = ['--task', 'quadrotor', '--algo', 'pid', '--overrides', './examples/tracking.yaml'] 
+    sys.argv[1:] = ['--algo', 'pid', '--task', 'quadrotor', '--overrides', './examples/tracking.yaml'] 
     tracking_run(gui=False, max_steps=10)
 
 def test_verbose_api():
-    sys.argv[1:] = ['--task', 'quadrotor', '--algo', 'pid', '--overrides', './examples/verbose_api.yaml']
+    sys.argv[1:] = ['--algo', 'pid', '--task', 'quadrotor', '--overrides', './examples/verbose_api.yaml']
     verbose_run()
--- tests/test_experiments/test_pid/test_pid.py
+++ tests/test_experiments/test_pid/test_pid.py
@@ -4,9 +4,9 @@ from experiments.pid.pid_experiment import run
 
 
 def test_pid_stabilization():
-    sys.argv[1:] = ['--task', 'quadrotor', '--algo', 'pid', '--overrides', './experiments/pid/config_overrides/quadrotor_stabilization.yaml'] 
+    sys.argv[1:] = ['--algo', 'pid', '--task', 'quadrotor', '--overrides', './experiments/pid/config_overrides/quadrotor_stabilization.yaml'] 
     run(gui=False, max_steps=10)
 
 def test_pid_trajectory_tracking():
-    sys.argv[1:] = ['--task', 'quadrotor', '--algo', 'pid', '--overrides', './experiments/pid/config_overrides/quadrotor_tracking.yaml'] 
+    sys.argv[1:] = ['--algo', 'pid', '--task', 'quadrotor', '--overrides', './experiments/pid/config_overrides/quadrotor_tracking.yaml'] 
     run(gui=False, max_steps=10)
