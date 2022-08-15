808f2f463c568e9b6b5ffd6bf55afd04555b56e5
Federico-PizarroBejarano <federico.pizarrobejarano@mail.utoronto.ca>
2022-08-15 10:59:21 -0400

    Adding independent config files for each task and system

--- experiments/lqr/lqr_experiment.sh
+++ experiments/lqr/lqr_experiment.sh
@@ -11,4 +11,4 @@ TASK='tracking'
 # ALGO='lqr'
 ALGO='ilqr'
 
-python3 ./lqr_experiment.py --task ${SYS} --algo ${ALGO} --overrides ./config_overrides/${SYS}_${TASK}.yaml ./config_overrides/${ALGO}_config.yaml
+python3 ./lqr_experiment.py --task ${SYS} --algo ${ALGO} --overrides ./config_overrides/${SYS}/${SYS}_${TASK}.yaml ./config_overrides/${SYS}/${ALGO}_${SYS}_${TASK}.yaml
--- tests/test_experiments/test_lqr/test_lqr.py
+++ tests/test_experiments/test_lqr/test_lqr.py
@@ -8,5 +8,5 @@ from experiments.lqr.lqr_experiment import run
 @pytest.mark.parametrize('TASK',['stabilization', 'tracking'])
 @pytest.mark.parametrize('ALGO',['lqr', 'ilqr'])
 def test_lqr(SYS, TASK, ALGO):
-    sys.argv[1:] = ['--algo', ALGO, '--task', SYS, '--overrides', f'./experiments/lqr/config_overrides/{SYS}_{TASK}.yaml', f'./experiments/lqr/config_overrides/{ALGO}_config.yaml'] 
+    sys.argv[1:] = ['--algo', ALGO, '--task', SYS, '--overrides', f'./experiments/lqr/config_overrides/{SYS}/{SYS}_{TASK}.yaml', f'./experiments/lqr/config_overrides/{SYS}/{ALGO}_{SYS}_{TASK}.yaml'] 
     run(gui=False, training=False, n_episodes=None, n_steps=10, save_data=False)
