6c45ccdc584d1db8670cdac3bce05bff06a2db63
Justin-Yuan <justin.zcyuan@gmail.com>
2022-09-02 16:30:51 -0400

    Use separate pid.yaml to specify prior config in pid experiment

--- experiments/pid/pid_experiment.sh
+++ experiments/pid/pid_experiment.sh
@@ -3,7 +3,7 @@
 # PID Experiment.
 
 ## Stabilization
-python3 ./pid_experiment.py --task quadrotor --algo pid --overrides ./config_overrides/quadrotor_stabilization.yaml
+python3 ./pid_experiment.py --task quadrotor --algo pid --overrides ./config_overrides/quadrotor_stabilization.yaml ./config_overrides/pid.yaml
 
 ## Tracking
-python3 ./pid_experiment.py --task quadrotor --algo pid --overrides ./config_overrides/quadrotor_tracking.yaml
+python3 ./pid_experiment.py --task quadrotor --algo pid --overrides ./config_overrides/quadrotor_tracking.yaml ./config_overrides/pid.yaml
