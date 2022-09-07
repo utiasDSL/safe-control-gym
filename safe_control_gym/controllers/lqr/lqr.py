43971cb3cf15bd4ba4b1468a0af53bfd1bad6b0d
Adam Hall <adam.wes.hall@gmail.com>
2022-09-07 00:38:06 -0400

    Updated to use model.U_EQ, otherwise we are using knowledge about the mass.

--- safe_control_gym/controllers/lqr/lqr.py
+++ safe_control_gym/controllers/lqr/lqr.py
@@ -39,7 +39,7 @@ class LQR(BaseController):
         self.env.set_cost_function_param(self.Q, self.R)
 
         if self.env.TASK == Task.STABILIZATION:
-            self.gain = compute_lqr_gain(self.model, self.env.X_GOAL, self.env.U_GOAL,
+            self.gain = compute_lqr_gain(self.model, self.model.X_EQ, self.model.U_EQ,
                                          self.Q, self.R, self.discrete_dynamics)
 
     def reset(self):
@@ -64,9 +64,9 @@ class LQR(BaseController):
         step = self.extract_step(info)
 
         if self.env.TASK == Task.STABILIZATION:
-            return -self.gain @ (obs - self.env.X_GOAL) + self.env.U_GOAL
+            return -self.gain @ (obs - self.model.X_EQ) + self.model.U_EQ
         elif self.env.TASK == Task.TRAJ_TRACKING:
             self.gain = compute_lqr_gain(self.model, self.env.X_GOAL[step],
-                                         self.env.U_GOAL, self.Q, self.R,
+                                         self.model.U_EQ, self.Q, self.R,
                                          self.discrete_dynamics)
-            return -self.gain @ (obs - self.env.X_GOAL[step]) + self.env.U_GOAL
+            return -self.gain @ (obs - self.env.X_GOAL[step]) + self.model.U_EQ
