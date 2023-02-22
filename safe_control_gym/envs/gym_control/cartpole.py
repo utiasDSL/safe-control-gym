d7d8c7a8e650410de2e3059127bee602aa1e959c
Adam Hall <adam.wes.hall@gmail.com>
2023-02-22 10:46:57 -0500

    Added self.CTRL_TIMESTEP to increment the trajectory by 1 where the trajectory is generated

--- safe_control_gym/envs/gym_control/cartpole.py
+++ safe_control_gym/envs/gym_control/cartpole.py
@@ -205,7 +205,7 @@ class CartPole(BenchmarkEnv):
             self.X_GOAL = np.hstack([self.TASK_INFO['stabilization_goal'][0], 0., 0., 0.])  # x = {x, x_dot, theta, theta_dot}.
         elif self.TASK == Task.TRAJ_TRACKING:
             POS_REF, VEL_REF, _ = self._generate_trajectory(traj_type=self.TASK_INFO['trajectory_type'],
-                                              traj_length=self.EPISODE_LEN_SEC,
+                                              traj_length=self.EPISODE_LEN_SEC + self.CTRL_TIMESTEP, # Added to increment the trajectory by 1 (PR #119)
                                               num_cycles=self.TASK_INFO['num_cycles'],
                                               traj_plane=self.TASK_INFO['trajectory_plane'],
                                               position_offset=np.array(self.TASK_INFO['trajectory_position_offset']),
