549ff3ece49d86952aa7eaeb5e1161c0685818a3
middleyuan <alex730310@gmail.com>
2023-07-11 15:47:10 +0200

    add a hpo parallelism test

--- experiments/comparisons/gpmpc/tests/test_hpo_gpmpc_cartpole.py
+++ experiments/comparisons/gpmpc/tests/test_hpo_gpmpc_cartpole.py
@@ -1,6 +1,7 @@
 import sys, os
 import pytest
 import munch
+import time
 
 
 from safe_control_gym.utils.configuration import ConfigFactory
@@ -17,6 +18,8 @@ def test_hpo_gpmpc_cartpole(SYS, TASK, ALGO, PRIOR):
         (create a study from scratch)
     '''
 
+    # drop the database if exists
+    drop(munch.Munch({'tag': 'gp_mpc_hpo'}))
     # create database
     create(munch.Munch({'tag': 'gp_mpc_hpo'}))
     # output_dir
@@ -50,39 +53,64 @@ def test_hpo_gpmpc_cartpole(SYS, TASK, ALGO, PRIOR):
 @pytest.mark.parametrize('TASK',['stab'])
 @pytest.mark.parametrize('ALGO',['gp_mpc'])
 @pytest.mark.parametrize('PRIOR',['150'])
-def test_hpo_gpmpc_cartpole(SYS, TASK, ALGO, PRIOR):
-    '''Test HPO for gp_mpc on cartpole stab task for one single trial
-        (create a study from scratch)
-    '''
+@pytest.mark.parametrize('LOAD', [False, True])
+def test_hpo_gpmpc_cartpole_parallelism(SYS, TASK, ALGO, PRIOR, LOAD):
+    '''Test HPO for gp_mpc on cartpole stab task in parallel.'''
 
-    # create database
-    create(munch.Munch({'tag': 'gp_mpc_hpo'}))
-    # output_dir
-    output_dir = './experiments/comparisons/gpmpc/results'
-    sys.argv[1:] = ['--algo', ALGO,
-                    '--task', SYS,
-                    '--overrides',
-                        f'./experiments/comparisons/gpmpc/config_overrides/{SYS}/{SYS}_{TASK}.yaml',
-                        f'./experiments/comparisons/gpmpc/config_overrides/{SYS}/{ALGO}_{SYS}_{PRIOR}.yaml',
-                        f'./experiments/comparisons/gpmpc/config_overrides/{SYS}/{ALGO}_{SYS}_hpo.yaml',
-                    '--output_dir', output_dir,
-                    ]
+    # if LOAD is False, create a study from scratch
+    if not LOAD:
+        # drop the database if exists
+        drop(munch.Munch({'tag': 'gp_mpc_hpo'}))
+        # create database
+        create(munch.Munch({'tag': 'gp_mpc_hpo'}))
+        # output_dir
+        output_dir = './experiments/comparisons/gpmpc/results'
+        sys.argv[1:] = ['--algo', ALGO,
+                        '--task', SYS,
+                        '--overrides',
+                            f'./experiments/comparisons/gpmpc/config_overrides/{SYS}/{SYS}_{TASK}.yaml',
+                            f'./experiments/comparisons/gpmpc/config_overrides/{SYS}/{ALGO}_{SYS}_{PRIOR}.yaml',
+                            f'./experiments/comparisons/gpmpc/config_overrides/{SYS}/{ALGO}_{SYS}_hpo.yaml',
+                        '--output_dir', output_dir,
+                        '--seed', '7',
+                        ]
 
-    fac = ConfigFactory()
-    fac.add_argument("--load_study", type=bool, default=False, help="whether to load study from a previous HPO.")
-    config = fac.merge()
-    config.algo_config.num_test_episodes_per_epoch = 1
-    config.algo_config.num_train_episodes_per_epoch = 1
-    config.hpo_config.trials = 1
+        fac = ConfigFactory()
+        fac.add_argument("--load_study", type=bool, default=False, help="whether to load study from a previous HPO.")
+        config = fac.merge()
+        config.hpo_config.trials = 1
 
-    hpo(config)
+        hpo(config)
+    # if LOAD is True, load a study from a previous HPO study
+    else:
+        # first, wait a bit untill the HPO study is created
+        time.sleep(3)
+        # output_dir
+        output_dir = './experiments/comparisons/gpmpc/results'
+        sys.argv[1:] = ['--algo', ALGO,
+                        '--task', SYS,
+                        '--overrides',
+                            f'./experiments/comparisons/gpmpc/config_overrides/{SYS}/{SYS}_{TASK}.yaml',
+                            f'./experiments/comparisons/gpmpc/config_overrides/{SYS}/{ALGO}_{SYS}_{PRIOR}.yaml',
+                            f'./experiments/comparisons/gpmpc/config_overrides/{SYS}/{ALGO}_{SYS}_hpo.yaml',
+                        '--output_dir', output_dir,
+                        '--seed', '8',
+                        ]
 
-    # delete output_dir
-    if os.path.exists(output_dir):
-        os.system(f'rm -rf {output_dir}')
+        fac = ConfigFactory()
+        fac.add_argument("--load_study", type=bool, default=True, help="whether to load study from a previous HPO.")
+        config = fac.merge()
+        config.hpo_config.trials = 1
 
-    # drop database
-    drop(munch.Munch({'tag': 'gp_mpc_hpo'}))
+        hpo(config)
+
+        # delete output_dir
+        if os.path.exists(output_dir):
+            os.system(f'rm -rf {output_dir}')
+
+        # drop database
+        drop(munch.Munch({'tag': 'gp_mpc_hpo'}))
+
 
 @pytest.mark.parametrize('SYS', ['cartpole'])
 @pytest.mark.parametrize('TASK',['stab'])
@@ -92,6 +120,8 @@ def test_train_gpmpc_cartpole(SYS, TASK, ALGO, PRIOR):
     '''Test training for gp_mpc on cartpole stab task given a set of hyperparameters.
     '''
 
+    # drop the database if exists
+    drop(munch.Munch({'tag': 'gp_mpc_hpo'}))
     # create database
     create(munch.Munch({'tag': 'gp_mpc_hpo'}))
     # output_dir
