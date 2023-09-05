5bca27aa044349aa4165ad794c7c04d0d525dd9f
middleyuan <alex730310@gmail.com>
2023-09-05 11:35:10 +0200

    fix the budget (trial) bug in configs.

--- experiments/comparisons/rl/main.sh
+++ experiments/comparisons/rl/main.sh
@@ -2,12 +2,12 @@
 
 cd ~/safe-control-gym
 
-hpo_runs=(1 2 3 4 5)
+hpo_runs=(1 2 3)
 # run HPO on with different strategy
 for run in "${hpo_runs[@]}"
 do
 
-bash experiments/comparisons/rl/rl_hpo_strategy.sh ${run} $((run+6)) TPESampler hostx sac cartpole stab
+bash experiments/comparisons/rl/rl_hpo_strategy.sh ${run} $((run)) TPESampler hostx sac cartpole stab
 
 done
 # eval the strategy
