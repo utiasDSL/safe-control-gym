82c7e189a358e83557935e02d6fdd8fde77e48f9
adamhall <adam.wes.hall@gmail.com>
2024-01-25 11:30:13 -0500

    MPC and Linear MPC examples

---
+++ examples/mpc/mpc_experiment.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# LQR Experiment.
+
+SYS='cartpole'
+#SYS='quadrotor_2D'
+#SYS='quadrotor_3D'
+
+#TASK='stabilization'
+TASK='tracking'
+
+#ALGO='mpc'
+ALGO='linear_mpc'
+
+if [ "$SYS" == 'cartpole' ]; then
+    SYS_NAME=$SYS
+else
+    SYS_NAME='quadrotor'
+fi
+
+python3 ./mpc_experiment.py \
+    --task ${SYS_NAME} \
+    --algo ${ALGO} \
+    --overrides \
+        ./config_overrides/${SYS}/${SYS}_${TASK}.yaml \
+        ./config_overrides/${SYS}/${ALGO}_${SYS}_${TASK}.yaml
