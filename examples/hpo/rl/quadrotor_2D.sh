b4508bba2e9b59b49db591ff6510ca4d03537660
middleyuan <alex730310@gmail.com>
2024-05-15 13:50:29 +0200

    edit quadrotor_2D.sh

--- examples/hpo/rl/quadrotor_2D.sh
+++ examples/hpo/rl/quadrotor_2D.sh
@@ -17,3 +17,17 @@ python ./examples/hpo/hpo_experiment.py \
 --overrides ./examples/hpo/rl/config_overrides/quadrotor_2D/quadrotor_2D_track.yaml \
             ./examples/hpo/rl/ppo/config_overrides/quadrotor_2D/ppo_quadrotor_2D_.yaml \
 --output_dir ./examples/hpo/results --n_episodes 10 --tag 2D --opt_hps '' --seed 6 --use_gpu True
+
+python ./examples/hpo/hpo_experiment.py \
+--algo sac \
+--task quadrotor \
+--overrides ./examples/hpo/rl/config_overrides/quadrotor_2D_attitude/quadrotor_2D_attitude_track.yaml \
+            ./examples/hpo/rl/sac/config_overrides/quadrotor_2D_attitude/sac_quadrotor_2D_attitude_.yaml \
+--output_dir ./examples/hpo/results --n_episodes 10 --tag 2D_attitude --opt_hps '' --seed 6 --use_gpu True
+
+python ./examples/hpo/hpo_experiment.py \
+--algo sac \
+--task quadrotor \
+--overrides ./examples/hpo/rl/config_overrides/quadrotor_2D/quadrotor_2D_track.yaml \
+            ./examples/hpo/rl/sac/config_overrides/quadrotor_2D/sac_quadrotor_2D_.yaml \
+--output_dir ./examples/hpo/results --n_episodes 10 --tag 2D --opt_hps '' --seed 6 --use_gpu True
