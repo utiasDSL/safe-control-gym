Fixing major error in cost calculation (#204)
* Fixing major error in cost calculation

* Fixing cost issue for cartpole as well

safe_control_gym/envs/gym_control/cartpole.py
@@ -19,6 +19,7 @@ import pybullet as p
 import pybullet_data
 from gymnasium import spaces
 
+from safe_control_gym.controllers.lqr.lqr_utils import get_cost_weight_matrix
 from safe_control_gym.envs.benchmark_env import BenchmarkEnv, Cost, Task
 from safe_control_gym.envs.constraints import GENERAL_CONSTRAINTS, SymmetricStateConstraint
 from safe_control_gym.math_and_models.normalization import normalize_angle
@@ -149,9 +150,9 @@ class CartPole(BenchmarkEnv):
         self.obs_goal_horizon = obs_goal_horizon
         self.obs_wrap_angle = obs_wrap_angle
         self.rew_state_weight = np.array(rew_state_weight, ndmin=1, dtype=float)
-        self.Q = np.diag(self.rew_state_weight)
         self.rew_act_weight = np.array(rew_act_weight, ndmin=1, dtype=float)
-        self.R = np.diag(self.rew_act_weight)
+        self.Q = get_cost_weight_matrix(self.rew_state_weight, 4)
+        self.R = get_cost_weight_matrix(self.rew_act_weight, 1)
         self.rew_exponential = rew_exponential
         self.done_on_out_of_bound = done_on_out_of_bound
         # BenchmarkEnv constructor, called after defining the custom args,

safe_control_gym/envs/gym_pybullet_drones/quadrotor.py
@@ -12,6 +12,7 @@ import numpy as np
 import pybullet as p
 from gymnasium import spaces
 
+from safe_control_gym.controllers.lqr.lqr_utils import get_cost_weight_matrix
 from safe_control_gym.envs.benchmark_env import Cost, Task
 from safe_control_gym.envs.constraints import GENERAL_CONSTRAINTS
 from safe_control_gym.envs.gym_pybullet_drones.base_aviary import BaseAviary
@@ -178,9 +179,7 @@ class Quadrotor(BaseAviary):
         self.norm_act_scale = norm_act_scale
         self.obs_goal_horizon = obs_goal_horizon
         self.rew_state_weight = np.array(rew_state_weight, ndmin=1, dtype=float)
-        self.Q = np.diag(self.rew_state_weight)
         self.rew_act_weight = np.array(rew_act_weight, ndmin=1, dtype=float)
-        self.R = np.diag(self.rew_act_weight)
         self.rew_exponential = rew_exponential
         self.done_on_out_of_bound = done_on_out_of_bound
         if info_mse_metric_state_weight is None:
@@ -562,6 +561,11 @@ class Quadrotor(BaseAviary):
             X_dot = cs.vertcat(pos_dot[0], pos_ddot[0], pos_dot[1], pos_ddot[1], pos_dot[2], pos_ddot[2], ang_dot, rate_dot)
 
             Y = cs.vertcat(x, x_dot, y, y_dot, z, z_dot, phi, theta, psi, p_body, q_body, r_body)
+
+        # Expand Q and R to be full matrices.
+        self.Q = get_cost_weight_matrix(self.rew_state_weight, nx)
+        self.R = get_cost_weight_matrix(self.rew_act_weight, nu)
+
         # Set the equilibrium values for linearizations.
         X_EQ = np.zeros(self.state_dim)
         U_EQ = np.ones(self.action_dim) * u_eq / self.action_dim